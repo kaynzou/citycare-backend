cc67ec6bcf1b9a084d68a65e52489f7110f4f65a
kaynzou <preetamk0069@gmail.com>
2026-08-30 10:49:28 +0530

    Add guaranteed demo phrases for major Indian languages

--- app/translation.py
+++ app/translation.py
@@ -37,6 +37,17 @@ PHRASE_DICTIONARY = {
     "park mein raat ko bahut shor hota hai": ("hi", "There is a lot of noise in the park at night."),
     "is ilake mein raat ko chori hoti hai": ("hi", "There are thefts in this area at night."),
     "bridge ka kaam bahut dinon se ruka hai": ("hi", "The bridge construction work has been stalled for many days."),
+    "আমার এলাকায় বিদ্যুৎ নেই": ("bn", "There is no electricity in my area."),
+    "எனது வீட்டிற்கு அருகில் குப்பை அகற்றப்படவில்லை": ("ta", "Garbage has not been removed near my house."),
+    "మా వీధిలో పెద్ద గొయ్యి ఉంది": ("te", "There is a big pothole in our street."),
+    "आमच्या रस्त्यावर मोठा खड्डा आहे": ("mr", "There is a big pothole on our road."),
+    "અમારા વિસ્તારમાં પાણી નથી": ("gu", "There is no water in our area."),
+    "ನಮ್ಮ ರಸ್ತೆಯಲ್ಲಿ ದೊಡ್ಡ ಗುಂಡಿ ಇದೆ": ("kn", "There is a big pothole on our road."),
+    "എന്റെ വീടിനടുത്ത് വലിയ കുഴി ഉണ്ട്": ("ml", "There is a big pothole near my house."),
+    "ਸਾਡੀ ਗਲੀ ਵਿੱਚ ਵੱਡਾ ਟੋਆ ਹੈ": ("pa", "There is a big pothole in our street."),
+    "ہماری گلی میں بڑا گڑھا ہے": ("ur", "There is a big pothole in our street."),
+    "ଆମ ରାସ୍ତାରେ ବଡ଼ ଗାତ ଅଛି": ("or", "There is a big pothole on our road."),
+    "আমাৰ পথত ডাঙৰ গাঁত আছে": ("as", "There is a big pothole on our road."),
 }
 
 
